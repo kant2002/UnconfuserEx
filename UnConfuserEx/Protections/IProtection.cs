244b971a9ac16722ba6419cab99592ea74ef2066
Andrii Kurdiumov <kant2002@gmail.com>
2026-04-30 20:21:32 +0200

    Fix publising files to artifacts

--- UnConfuserEx/Protections/IProtection.cs
+++ UnConfuserEx/Protections/IProtection.cs
@@ -4,6 +4,9 @@ namespace UnConfuserEx.Protections;
 
 internal interface IProtection
 {
+    /// <summary>
+    /// Gets name of the protection which is being detected/removed. This is used for logging purposes.
+    /// </summary>
     string Name { get; }
 
     public bool IsPresent(ref ModuleDefMD module);
